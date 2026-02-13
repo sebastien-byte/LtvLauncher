9e22f095728c7e8e920bfb675d053a0dbfcdfb82
akitothemoon <183810652+akitothemoon@users.noreply.github.com>
2026-02-13 19:02:24 +0530

    feat: implement time-based wallpaper switching

--- lib/widgets/settings/wallpaper_panel_page.dart
+++ lib/widgets/settings/wallpaper_panel_page.dart
@@ -16,12 +16,15 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'package:flauncher/providers/settings_service.dart';
 import 'package:flauncher/providers/wallpaper_service.dart';
 import 'package:flauncher/widgets/settings/focusable_settings_tile.dart';
 import 'package:flauncher/widgets/settings/gradient_panel_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:flauncher/widgets/rounded_switch_list_tile.dart';
 
 class WallpaperPanelPage extends StatelessWidget {
   static const String routeName = "wallpaper_panel";
@@ -34,35 +37,78 @@ class WallpaperPanelPage extends StatelessWidget {
         children: [
           Text(localizations.wallpaper, style: Theme.of(context).textTheme.titleLarge),
           Divider(),
-          FocusableSettingsTile(
-            autofocus: true,
-            leading: Icon(Icons.gradient),
-            title: Text(localizations.gradient, style: Theme.of(context).textTheme.bodyMedium),
-            onPressed: () => Navigator.of(context).pushNamed(GradientPanelPage.routeName),
+          Consumer<SettingsService>(
+            builder: (_, settings, __) {
+              return RoundedSwitchListTile(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(localizations.timeBasedWallpaper),
+                    Text(localizations.timeBasedWallpaperDescription, style: Theme.of(context).textTheme.bodySmall),
+                  ],
+                ),
+                secondary: Icon(Icons.access_time),
+                value: settings.timeBasedWallpaperEnabled,
+                onChanged: (value) => settings.setTimeBasedWallpaperEnabled(value),
+              );
+            }
           ),
-          FocusableSettingsTile(
-            leading: Icon(Icons.insert_drive_file_outlined),
-            title: Text(localizations.picture, style: Theme.of(context).textTheme.bodyMedium),
-            onPressed: () async {
-              try {
-                await context.read<WallpaperService>().pickWallpaper();
-              } on NoFileExplorerException {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    duration: Duration(seconds: 8),
-                    content: Row(
-                      children: [
-                        Icon(Icons.error_outline, color: Colors.red),
-                        SizedBox(width: 8),
-                        Text(localizations.dialogTextNoFileExplorer)
-                      ],
+          Consumer<SettingsService>(
+            builder: (_, settings, __) {
+              if (settings.timeBasedWallpaperEnabled) {
+                return Column(
+                  children: [
+                    FocusableSettingsTile(
+                      leading: Icon(Icons.wb_sunny),
+                      title: Text(localizations.pickDayWallpaper),
+                      onPressed: () => _pickWallpaper(context, (s) => s.pickWallpaperDay(), localizations),
                     ),
-                  ),
+                    FocusableSettingsTile(
+                      leading: Icon(Icons.nights_stay),
+                      title: Text(localizations.pickNightWallpaper),
+                      onPressed: () => _pickWallpaper(context, (s) => s.pickWallpaperNight(), localizations),
+                    ),
+                  ],
+                );
+              } else {
+                return Column(
+                  children: [
+                    FocusableSettingsTile(
+                      autofocus: true,
+                      leading: Icon(Icons.gradient),
+                      title: Text(localizations.gradient, style: Theme.of(context).textTheme.bodyMedium),
+                      onPressed: () => Navigator.of(context).pushNamed(GradientPanelPage.routeName),
+                    ),
+                    FocusableSettingsTile(
+                      leading: Icon(Icons.insert_drive_file_outlined),
+                      title: Text(localizations.picture, style: Theme.of(context).textTheme.bodyMedium),
+                      onPressed: () => _pickWallpaper(context, (s) => s.pickWallpaper(), localizations),
+                    ),
+                  ],
                 );
               }
-            },
+            }
           ),
         ],
+    );
+  }
+
+  Future<void> _pickWallpaper(BuildContext context, Future<void> Function(WallpaperService) action, AppLocalizations localizations) async {
+    try {
+      await action(context.read<WallpaperService>());
+    } on NoFileExplorerException {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: Duration(seconds: 8),
+          content: Row(
+            children: [
+              Icon(Icons.error_outline, color: Colors.red),
+              SizedBox(width: 8),
+              Text(localizations.dialogTextNoFileExplorer)
+            ],
+          ),
+        ),
       );
+    }
   }
 }
